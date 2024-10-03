69e66696ea8ec7fdb5126e332c5200e649706f2d
dagriffith0616 <55294475+dagriffith0616@users.noreply.github.com>
2024-10-03 11:02:46 -0400

    Add files via upload

---
+++ SnakedPython/snake.py
@@ -0,0 +1,58 @@
+from turtle import Turtle, Screen
+import time
+MOVE_DISTANCE = 20
+
+
+
+POSITION = [(0, 0), (-20, 0), (-40, 0)]
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+    def create_snake(self):
+        for p in POSITION:
+            self.add_segment(p)
+
+
+    def move(self):
+        for segment in range(len(self.segments)-1, 0, -1):
+            new_x = self.segments[segment - 1].xcor()
+            new_y = self.segments[segment - 1].ycor()
+            self.segments[segment].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+
+    def add_segment(self, position):
+        new_snake = Turtle('square')
+        new_snake.color('white')
+        new_snake.penup()
+        new_snake.goto(position)
+        self.segments.append(new_snake)
+
+    def reset(self):
+        for segment in self.segments:
+            segment.goto(-500, 500)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
